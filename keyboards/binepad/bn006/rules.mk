f181ced2e46e19fb5a84d27572d51551b75e29a5
itsjonny96 <122654221+itsjonny96@users.noreply.github.com>
2023-06-21 08:53:27 -0400

    Adds the master map for the BN006
    Bare bones keymap for BN006, missing some layers. VIA enabled in rules file
    to enable DFU mood as a fail safe. DFU mapped to 4 key combo

--- keyboards/binepad/bn006/rules.mk
+++ keyboards/binepad/bn006/rules.mk
@@ -1,1 +1,9 @@
-# This file intentionally left blank
+# Copyright 2022 Binepad (@binpad)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+EPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
+
+COMBO_ENABLE = yes
+COMBO_COUNT = 1
+VIA_ENABLE = yes
