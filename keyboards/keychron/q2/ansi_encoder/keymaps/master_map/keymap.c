1900851324d43cb65d4ba063bf37bf7c2ed4a500
itsjonny96 <122654221+itsjonny96@users.noreply.github.com>
2024-01-06 18:59:18 -0500

    Adding lighting to keychron master map
    Adds the caps lock and key indicator lighting for the layers to
    the master map of q2

--- keyboards/keychron/q2/ansi_encoder/keymaps/master_map/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/master_map/keymap.c
@@ -97,6 +97,30 @@ combo_t key_combos[] = {
 
 //LIGHTING
 
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
+                rgb_matrix_set_color(i, RGB_RED);
+            }
+        }
+    }
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index < led_max && index != NO_LED &&
+                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_BLUE);
+                }
+            }
+        }
+    }
+    return false;
+}
 
 // KEYMAP
 
